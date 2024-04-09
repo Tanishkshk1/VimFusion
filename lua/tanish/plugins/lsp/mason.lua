e5137f2bc346d35ea357050c641c9ebe3b25a5f4
Tanishkshk1 <2821108.cse@pietgroup.co.in@gmail.com>
2024-04-09 20:40:30 +0530

    Adding screenshots

--- lua/tanish/plugins/lsp/mason.lua
+++ lua/tanish/plugins/lsp/mason.lua
@@ -33,7 +33,6 @@ return {
         "tailwindcss",
         "svelte",
         "lua_ls",
-        "graphql",
         "emmet_ls",
         "prismals",
         "pyright",
@@ -43,9 +42,9 @@ return {
     mason_tool_installer.setup({
       ensure_installed = {
         "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
+        "stylua",   -- lua formatter
+        "isort",    -- python formatter
+        "black",    -- python formatter
         "pylint",
         "eslint_d",
       },
