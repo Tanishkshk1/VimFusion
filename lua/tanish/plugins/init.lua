180f5001afefc4551e40fe9f2b1488b97ed5535b
tanishkshk1 <2821108.cse@pietgroup.co.in@gmail.com>
2024-07-11 20:58:45 +0530

    removed some bus

--- lua/tanish/plugins/init.lua
+++ lua/tanish/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
 	"nvim-lua/plenary.nvim",
+	"mbbill/undotree",
 	"christoomey/vim-tmux-navigator",
 }
--- lua/tanish/plugins/which-key.lua
+++ lua/tanish/plugins/which-key.lua
@@ -1,11 +1,9 @@
-return{
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timelen = 500
-  end,
-  opts = {
-  },
-
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		--     vim.o.timelen = 500
+	end,
+	opts = {},
 }
